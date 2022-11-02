86d9b5c4bb246c624e4b839032197daeb4441f6c
cy-shin <chimakpi0804@gmail.com>
2022-11-02 16:48:39 +0900

    1104 js 변수, 자료형

---
+++ 3_Javascript/js/04_변수와자료형.js
@@ -0,0 +1,162 @@
+var str = "JS 코드를 함수가 아닌 "
+        + "JS 파일 또는 script 태그 내부에 바로 작성하면"
+        + "HTML 렌더링 시 바로 수행된다.";
+
+console.log(str);
+// alert(str);
+
+// 변수 선언 위치에 따른 구분
+var num1 = 100; // 전역 변수
+num2 = 200;     // 전역 변수
+
+console.log("num1 : " + num1);
+console.log("num2 : " + num2);
+
+function test() {
+    var num3 = 300; // function 지역 변수
+    num4 = 400;     // 전역 변수
+
+    console.log("함수 내부 num3 : " + num3);
+    console.log("함수 내부 num4 : " + num4);
+
+    if(num3 == 300) {
+        var num5 = 500;
+        num6 = 600;
+    }
+
+    console.log("if문 종료 후 num5 : " + num5);
+    console.log("if문 종료 후 num6 : " + num6);
+}
+
+// 함수 호출
+test();
+
+// console.log("함수 외부 num3 : " + num3);
+// 정의되지 않은 함수(num3 is not defined)
+// 함수 내에서 선언된 변수여서, 함수 종료 후 사용 불가
+
+// *** script 언어는 중간에 에러가 발생하면 해석을 멈춤 ***
+
+console.log("함수 외부 num4 : " + num4); 
+// 전역 변수
+// 함수 종료 후에도 사용할 수 있음
+
+// console.log("if문 종료 후 num5 : " + num5);
+console.log("if문 종료 후 num6 : " + num6); // 전역 변수
+
+// var 변수명 중복 확인
+var num1 = 999;
+console.log("var num1 중복 확인 : " + num1);
+
+// 자료형 확인 (+변수 선언 방법)
+function typeTest(){
+
+    let temp1 = 100;
+    // let temp1 = 100; 변수명 중복 X
+    // 블록 범위 변수 'temp1'을(를) 다시 선언할 수 없습니다.ts(2451)
+
+    if(temp1 == 100) {
+        let temp2 = 200;  
+        const temp3 = 300;
+         // temp3 = 400; 
+         // 상수이므로 값 변경 불가
+         // Assignment to constant variable.
+    }
+
+    console.log(temp1);
+    // console.log(temp2); // is not defined
+    // console.log(temp3); // is not defined
+
+
+
+
+    // 자료형 확인
+    const typeBox = document.getElementById("typeBox");
+
+    typeBox.innerHTML = ""; // 내부 내용 삭제
+
+    // const name2;
+
+    let name; // undefined
+    typeBox.innerHTML += "name : " + name + " / " + typeof name;
+    
+    name = "홍길동"; // String
+
+    // typeof 변수명 : 해당 변수의 자료형을 반환하는 연산자
+    typeBox.innerHTML += "<br><br>name : " + name + " / " + typeof name;
+
+    // number
+    const age = 20;
+    const height = 178.9;
+
+    typeBox.innerHTML += "<br><br>age : " + age + " / " + typeof age;
+    typeBox.innerHTML += "<br><br>height : " + height + " / " + typeof height;
+
+    // boolean
+    const isTrue = true;
+    typeBox.innerHTML += "<br><br>isTrue : " + isTrue + " / " + typeof isTrue;
+
+    // object(배열, JS객체)
+
+    // 배열 선언과 동시에 초기화
+    // (Java)
+    // int[] arr = {10, 20, 30, 40, 50};
+
+    // JS ([] 사용)
+    const arr = [10, 20, 30, 40, 50];
+
+    typeBox.innerHTML += "<br><br>arr : " + arr + " / " + typeof arr;
+
+    for(let i=0; i<arr.length; i++) {
+        typeBox.innerHTML += "<br>arr[ "+ i + "] : " + arr[i];
+    }
+
+    // ** JS 객체 **
+    // - K:V 쌍으로 이루어짐(Map 형식)
+    // - Key는 무적권 String
+    // - 리터럴 표기법 : {}
+
+    // const user = { "K":V, "K":V };
+    const user = { "id":"user01", "pw":"pass01", "num":123, "a":arr };
+    typeBox.innerHTML += "<br><br>user : " + user + " / " + typeof user; 
+    console.log(user); // 콘솔에서 확인 가능함!
+
+    // 객체에 저장된 값 출력
+    typeBox.innerHTML += "<br>user.id : " + user.id;
+    typeBox.innerHTML += "<br>user.pw : " + user.pw;
+    typeBox.innerHTML += "<br>user.num : " + user.num;
+    typeBox.innerHTML += "<br>user.a : " + user.a;
+    typeBox.innerHTML += "<br>user.a[2] : " + user.a[2];
+
+    // function
+    // function(n1, n2) { // 익명 함수
+    //     return n1 + n2;
+    // }
+    const fnSum = function(n1, n2) { // 변수명을 함수명으로 사용
+        return n1 + n2;
+    }
+    // 출력 구문에 함수명만 작성한 경우 -> 함수에 작성된 코드 자체가 반환됨
+    typeBox.innerHTML += "<br><br>fnSum : " + fnSum + " / " + typeof fnSum;
+    
+    // 함수명(매개변수) == 함수 호출 { 함수 코드가 수행됨 }
+    typeBox.innerHTML += "<br><br>fnSum(1, 2) : " + fnSum(1, 2) + " / " + typeof fnSum;
+
+    // 2) 함수를 매개변수로 전달하는 형태
+
+    function plusFn(num, fn) {
+        // num : number
+        // fn : function
+
+        return num + fn() 
+    }
+
+    const result = plusFn(100, function(){ return 30 - 10;  })
+    
+    typeBox.innerHTML += "<br><br>result : " + result;
+
+
+}
+
+
+
+
