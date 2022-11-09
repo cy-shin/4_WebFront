2872b4a756d1c9cc6a2e5773faab5aa35d7d8d37
cy-shin <chimakpi0804@gmail.com>
2022-11-09 16:22:25 +0900

    1109 JS 객체 + JSON 기초

--- 3_Javascript/js/12_JS객체.js
+++ 3_Javascript/js/12_JS객체.js
@@ -76,6 +76,7 @@ document.getElementById("btn1").addEventListener("click",function(){
     // 객체가 가지고 있는 모든 값 출력하기
     // -> for in 구문(객체 전용 for문)
     div1.innerHTML += "product['capacity'] : " + product['capacity'] + "<br>";
+    div1.innerHTML += "<hr>";
 
 
     // for(let key in 객체명) : 매 바퀴마다 객체의 K(key)를 하나씩 얻어와
@@ -87,4 +88,80 @@ document.getElementById("btn1").addEventListener("click",function(){
         div1.innerHTML += product[key] + "<br>";
     }
 
+})
+
+
+
+document.getElementById("btn2").addEventListener("click", function(){
+
+    const div2 = document.getElementById("div2");
+    const stdList = []; //  비어있는 배열
+
+    // 2. 생성자 함수 호출 : new 생성자 함수명(매개변수)
+    // 비어있는 배열 stdList에 push를 이용해 값을 대입 == 생성자 함수 호출
+    stdList.push(new Student(1, 2, "홍길동"));
+    stdList.push(new Student(3, 10, "김길동"));
+    stdList.push(new Student(2, 5, "최길동"));
+    stdList.push(new Student(4, 7, "박철수"));
+
+    div2.innerHTML = "";
+
+    // for of : 배열, 컬렉션 요소 반복 접근용 for문
+    for(let std of stdList){
+        for(let key in std){
+            // div2.innerHTML += key + " : " + std[key] + "<br>";
+            // std[key]의 자료형이 'function'이면 실행 후 출력
+            if(typeof std['key'] == 'function') {
+                div2.innerHTML += key + " : " + std[key]() + "<br>";
+            } else {
+                div2.innerHTML += key + " : " + std[key] + "<br>"
+            }
+
+        }
+        div2.innerHTML += "<hr>"
+    }
+
+    console.log(stdList);
+});
+
+// 1. 생성자 함수 정의
+// (1). function 함수명(){} : 함수명의 첫 글자는 반드시 대문자이어야 함
+function Student(grade, ban, name){ 
+    // (2). JS 함수의 매개변수에는 유형(let, const, var)를 적지 않음
+    // -> 적지 않았지만 해당 함수의 지역변수로 취급됨
+
+    // 생성자 함수에서의 this 
+    // == new 연산자를 통해 만들어지는 객체
+    // ** 객체 내부에서 this == 현재 객체(const product) **
+
+    // 속성
+    this.grade = grade;
+    // 매개변수 grade 값을 생성되는 객체의 grade에 대입함
+    // 생성될 객체의 grade = 매개변수 grade
+
+    this.ban = ban;
+    this.name = name;
+
+    // 기능
+    this.introduce = function(){
+        // return grade <- 이때 grade는 객체의 매개변수 grade를 의미
+        return this.grade + "학년 " + this.ban + "반 " + this.name;
+        // return this.grade <- 이때 this.grade는 앞으로 만들어질 객체의 grade를 의미
+    }
+}
+
+// JSON
+document.getElementById("btn3").addEventListener("click", function(){
+    // JSON.stringify(JS객체) : JS객체     -> JSON문자열 변환
+    // JSON.parse(JSON문자열) : JSON문자열 -> JS 객체 변환
+
+    const std = new Student(5, 7, "샘오칠이");
+    console.log(std); // 객체
+    console.log(JSON.stringify(std)); // 객체가 JSON으로 변환됨
+    // ** JSON 변환 시 객체의 기능은 포함하지 않는다!!!!!
+    // -> JSON은 데이터 전달에 특화되어 있음
+    console.log("-------------------");
+    const member = '{"memberId":"user01","memberPw":"pass01","age":20}';
+    console.log(member); // JSON 문자열
+    console.log(JSON.parse(member)); // 을 객체로 만들기
 })
